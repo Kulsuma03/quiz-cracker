4c45c1645cf9561dac620649fbb69a8b7e764eb4
Kulsuma Akter <kulsumams72@gmail.com>
2022-10-11 22:28:37 +0600

    toast problem solved

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { EyeIcon } from '@heroicons/react/24/solid'
 import Swal from 'sweetalert2'
@@ -7,30 +7,33 @@ import { json } from 'react-router-dom';
 
 const Question = ({ questions }) => {
 
-    const { options, question, correctAnswer } = questions;
+    const { options, question, correctAnswer ,id } = questions;
     const question1 = question.split('<p>');
     const question2 = (question1[1]).split('</p>')
-
-
-    console.log(questions);
+    
+   
 
     const handleAns = (option) => {
+        
         if (option === correctAnswer) {
-            toast.success(' Great!!! Your Answer is Correct', { autoClose: 1000 })
+            toast.success(' Great!!! Your Answer is Correct', { autoClose: 1000 });
+            
         } else {
-            toast.warning('Ops!!! Your Answer is Wrong', { autoClose: 1000 })
+            
+            toast.warning('Ops!!! Your Answer is Wrong', { autoClose: 1000 });
+           
         }
     }
-
+    
     const handleCorrectAns = (question) => {
         
         const answer = question.correctAnswer;
-        console.log(answer)
+        // console.log(answer)
         toast.success(answer, {autoClose: 1000})
-
-
+        
+         
         // Swal.fire({
-        //     title: `${answer}`,
+        //     title: (`<p>Correct Ansewer is: </p>  <p>${answer}</p>`),
         //     showClass: {
         //       popup: 'animate__animated animate__fadeInDown'
         //     },
@@ -54,10 +57,10 @@ const Question = ({ questions }) => {
                 </div>
                 <div className='grid gap-4 lg:grid-cols-2 mt-9   mx-auto'>
                     {
-                        options.map(option =>
-                            <div key={option} className='flex my-3  items-start'>
-                                <input className='mt-2 mr-1' type="radio" name="drone" />
-                                <p className='mt-0' onClick={() => handleAns(option)} >{option}</p>
+                        options.map((option, index) =>
+                            <div key={index} className='flex my-3  items-start'>
+                                <input className='mt-2 mr-1' type="radio" id={id} name={id} />
+                                <label  className='mt-0' onClick={() => handleAns(option)} >{option}</label>
                             </div>
                         )
                     }
